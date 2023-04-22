0efdfad17b9ab15f570ffb42c8e95490874b821b
Cristofher Pablo <chatcomchris@gmail.com>
2023-04-22 11:24:20 -0300

    Create: Basic setup to use Webpack in the project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/style.css';
+import icon from './assets/yolk.ico';
+
+const iconImg = document.querySelector(`link[rel~='icon']`);
+iconImg.href = icon;
+
+console.log('1 2 3');
+console.log('clean');
