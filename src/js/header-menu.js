d6ab98c0298ef79d8a5a761bd46892378faaeee6
Cristofher Pablo <chatcomchris@gmail.com>
2023-04-23 16:57:49 -0300

    Fix: minor changes in the logo section to make it all clickable

--- src/js/header-menu.js
+++ src/js/header-menu.js
@@ -4,7 +4,7 @@ import logoImg from '../assets/logo.png';
 
 
 var headerMenu = (function () {
-  var content, header, logo, ul, liHome, liMenu, liContact, logoTxt, a, divLogo;
+  var content, header, logo, ul, liHome, liMenu, liContact, logoTxt, aLogo;
 
   function init() {
     cacheDOM();
@@ -15,8 +15,7 @@ var headerMenu = (function () {
     content = document.querySelector('.content');
     header = new DOMConstructor('header', 'headerMenu', null, null);
     //logo elements 
-    divLogo = new DOMConstructor('div', null, ['headerLogo'], null);
-    a = new DOMConstructor('a', 'logoHome', null, null);
+    aLogo = new DOMConstructor('a', 'logoHome', ['headerLogo'], null);
     logo = new DOMConstructor('img', null, ['logoImg'], null);
     logoTxt = new DOMConstructor('p', null, null, `Funny Donuts'`);
     // nav elements 
@@ -29,14 +28,12 @@ var headerMenu = (function () {
 
   function render() {
     // Setting the Logo Img and text
-    divLogo = divLogo.setElement();
     logo = logo.setElement();
     logo.setAttribute('src', logoImg);
     logoTxt = logoTxt.setElement();
-    a = a.setElement();
-    a.appendChild(logo);
-    divLogo.appendChild(a);
-    divLogo.appendChild(logoTxt);
+    aLogo = aLogo.setElement();
+    aLogo.appendChild(logo);
+    aLogo.appendChild(logoTxt);
 
     //Setting the navbar menu 
     ul = ul.setElement();
@@ -46,7 +43,7 @@ var headerMenu = (function () {
 
     // append it all to the header 
     header = header.setElement();
-    header.appendChild(divLogo);
+    header.appendChild(aLogo);
     header.appendChild(ul);
 
     // appending the header to the content container
