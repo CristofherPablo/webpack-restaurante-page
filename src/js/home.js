d9a06a202d9af6b0414c30986e85ae8cfc68a726
Cristofher Pablo <chatcomchris@gmail.com>
2023-04-23 21:00:21 -0300

    Fix: create css global variables

--- src/js/home.js
+++ src/js/home.js
@@ -2,12 +2,10 @@ import DOMConstructor from './DOMConstructor';
 import headerMenu from './header-menu';
 
 function testingCreateElement() {
-  const div = new DOMConstructor('div', null, ['test'], null);
-
-  const container = document.querySelector('.content');
-    
-  container.appendChild(div.setElement());
+  
   headerMenu.init();
 }
+
+
 
 export default testingCreateElement;
